2574154a4bf2300cefeb203253c99f7fe009d3c5
hanzala2626 <hanzalaansari26@gmail.com>
2024-09-28 19:13:47 +0530

    ui changes in home pafhe

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,18 @@ function App() {
   return (
     <>
       <div style={{display:"flex",alignItems:"center",justifyContent:"center",height:"full",width:"full"}}>
-      <h1 >Salim bhai</h1>
+      <h1 style={{display:"flex",alignItems:"center",gap:"1rem"}}>Salim bhai {" "}
+      <span style={{
+        fontSize:"4rem",
+  background: 'linear-gradient(to right, red, green)',
+  WebkitBackgroundClip: 'text',
+  WebkitTextFillColor: 'transparent'
+}}>
+  BhandWa
+</span>
+
+
+         </h1>
       </div>
       
     </>
